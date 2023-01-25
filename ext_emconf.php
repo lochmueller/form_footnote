947d9d54ee442e193db40e932491dbb1b9cdadca
Tim Lochmüller <tim.lochmueller@hdnet.de>
2023-01-25 11:37:00 +0100

    Add PHP Version constraint

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,6 +10,7 @@ $EM_CONF[$_EXTKEY ?? 'form_footnote'] = [
     'version' => '1.2.1',
     'constraints' => [
         'depends' => [
+            'php' => '8.0.0-8.2.99',
             'typo3' => '11.5.0-11.5.99',
             'form' => '11.5.0-11.5.99',
         ],
